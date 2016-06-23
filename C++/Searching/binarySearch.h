381815b4705d07af1c6146492779f41a530a5a48
vishnu4v5 <vishnu4v5@gmail.com>
2016-06-22 22:41:07 -0400

    Adding initial binary search

---
+++ C++/Searching/binarySearch.h
@@ -0,0 +1,15 @@
+/* *******************************************************************
+ Binary search implementation  
+******************************************************************** */
+
+#ifndef BINARYSEARCH_H
+#define BINARYSEARCH_H
+
+template<typename T>
+class LinearSearch
+{
+public:
+	int static Search(T array[], int length, T data);
+};
+
+#endif
