546340d5eb8c9cb864e935aec7b2570ef16bba00
vishnu4v5 <vishnu4v5@gmail.com>
2016-07-09 23:51:33 -0400

    Insertion sort: refactored

--- C++/Sorting/insertionSort.cpp
+++ C++/Sorting/insertionSort.cpp
@@ -1,43 +1,69 @@
+/* *******************************************************************
+ Insertion sort implementation  
+******************************************************************** */
+
 #include "insertionSort.h"
 #include "common.h"
 #include <string>
 // #include <iostream>
 // using namespace std;
 
+
+//--------------------------------------------------------------------
+// Function: To sort elements of an arrays using insertion sort
+//--------------------------------------------------------------------
 template<typename T>
 T *InsertionSort<T>::Sort(T array[], int len, bool type)
 {
 	int i, j;
 	T temp;
+
+	// ascending sort
 	if (type)
 	{
+		// start from the second element
 		for (i = 1; i < len; i++)
 		{
 			j = i - 1;
+			
+			// store current element in temp location
 			temp = array[i];
+
+			// check wether temp element is smaller than previous elements
 			while (temp < array[j] && j >= 0)
 			{
+				// if so swap advance elements one place forward
 				array[j+1] = array[j];
 				j--;
 			}
+			// place the temp element in the ascending order
 			array[j+1] = temp;
+			
 			// for debuggin
 			// for (int k = 0; k < len; k++)
 			// 	cout << array[k] << " ";
 			// cout << endl;
 		}
 	}
+
+	// descending sort
 	else
 	{
+		// start from the second element
 		for (i = 1; i < len; i++)
 		{
 			j = i - 1;
+			
 			temp = array[i];
+
+			// check wether temp element is larger than previous elements
 			while (temp > array[j] && j >= 0)
 			{
+				// if so swap advance elements one place forward
 				array[j+1] = array[j];
 				j--;
 			}
+			// place the element in the descending order
 			array[j+1] = temp;
 		}
 	}
