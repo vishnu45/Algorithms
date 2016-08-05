d420f60bfd995fd220faf345fa8d2b77b5f06467
vishnu4v5 <vishnu4v5@gmail.com>
2016-08-05 09:41:17 -0400

    Adding QuickSort: initial commit

---
+++ C++/Sorting/quickSort.cpp
@@ -0,0 +1,6 @@
+#include "quickSort.h"
+
+int * QuickSort::Sort(int * array, int length, bool type)
+{
+	return array;
+}
---
+++ C++/Sorting/quickSort.h
@@ -0,0 +1,10 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+class QuickSort
+{
+public:
+	int static *Sort(int *array, int len, bool type);
+};
+
+#endif
