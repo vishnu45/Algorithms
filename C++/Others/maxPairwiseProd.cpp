334d6a7ee5d1efbbe20739ba8af115aa5607c88a
vishnu4v5 <vishnu4v5@gmail.com>
2016-08-07 04:04:30 -0400

    Updating O.maxPairProduct: improved appraoch

--- C++/Others/maxPairwiseProd.cpp
+++ C++/Others/maxPairwiseProd.cpp
@@ -21,6 +21,46 @@ int MaxProd (vector<int> &vec)
 	return product;
 }
 
+// improved function to find pair wise max product
+int ImpMaxProd (vector<int> &vec)
+{
+	// big and small are two of the largest numbers in the group
+	int big, small;
+	if (vec[0] > vec[1])
+	{
+		big = vec[0];
+		small = vec[1];
+	}
+	else
+	{
+		big = vec[1];
+		small = vec[0];
+	}
+
+	int product;
+	int count = vec.size();
+
+	// scan through the rest of the elements in the group
+	for (int i=2 ; i<count; i++)
+	{
+		if (vec[i] > big)
+		{
+			if (vec[i] > small)
+			{
+				small = big;
+				big = vec[i]; 
+			}
+			else
+				small = vec[i];
+		}
+		else if (vec[i] > small)
+			small = vec[i];
+	}
+	product = big * small;
+
+	return product;
+}
+
 int main()
 {
 	int count;	
@@ -30,7 +70,7 @@ int main()
 	for (int i = 0; i < count; i++)
 		cin >> vec[i];
 
-	cout << "Product = " << MaxProd(vec);
+	cout << "Product = " << ImpMaxProd(vec);
 
 	return 0;
 }
